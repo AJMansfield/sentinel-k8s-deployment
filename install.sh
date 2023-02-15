a42b3002cac66b6fe5564465fc7b116740bcf558
Anson Mansfield <amansfield@mantaro.com>
2023-02-15 15:28:49 +0000

    refresh an existing install

--- install.sh
+++ install.sh
@@ -3,17 +3,16 @@
 set -e -x
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-curl -sfL https://get.rke2.io | sudo sh -
+# install RKE2 if not already installed
+[ -f /usr/local/bin/rke2 ] || { curl -sfL https://get.rke2.io | sudo sh - }
 
+# install our custom configurations
 sudo cp --interactive --verbose --backup --recursive "$script_path/system/." "/."
 
-sudo systemctl enable rke2-server.service
+# enable and start the system units
+systemctl is-enabled --quiet rke2-server.service sudo systemctl enable rke2-server.service
+systemctl is-active --quiet rke2-server.service || sudo systemctl start rke2-server.service
 
-# sudo journalctl --follow --unit=rke2-server.service --output=cat &
-# journalctl_job=$!
-# function finish {
-#   kill $journalctl_job
-# }
-# trap finish EXIT
-
-sudo systemctl start rke2-server.service
+# copy authentication for installed server to user's config
+cp --backup --force ~/.kube/config ~/.kube/config
+sudo cat /etc/rancher/rke2/rke2.yaml | tee ~/.kube/config
