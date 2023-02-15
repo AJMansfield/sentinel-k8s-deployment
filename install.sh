7a781688f961b31d3cb9029eb1b809307eec5c7e
Anson Mansfield <amansfield@mantaro.com>
2023-02-15 15:05:01 +0000

    fix install paths

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 set -e -x
-script_path==$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-sudo cp --interactive --verbose --backup --recursive "$script_path/system/" "/"
+script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 curl -sfL https://get.rke2.io | sudo sh -
 
+sudo cp --interactive --verbose --backup --recursive "$script_path/system/." "/."
+
 sudo systemctl enable rke2-server.service
+
+# sudo journalctl --follow --unit=rke2-server.service --output=cat &
+# journalctl_job=$!
+# function finish {
+#   kill $journalctl_job
+# }
+# trap finish EXIT
+
 sudo systemctl start rke2-server.service
