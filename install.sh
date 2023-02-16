a51bc43a09a1e8fa85f1eaa3250a1ef79528a3f4
Anson Mansfield <amansfield@mantaro.com>
2023-02-16 14:14:18 +0000

    fix looping

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 set -e -x
-script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+pkg_root="${script_dir}/system"
+sys_root="${1:-}"
 
 # install RKE2 if not already installed
+# TODO pass sys_root to the RKE2 install script too
 [ -f /usr/local/bin/rke2 ] || { curl -sfL https://get.rke2.io | sudo sh - ; }
 
 # install our custom kubernetes manifest files
-sudo install --verbose \
-    --owner=root --group=root --mode=644 \
-    --recursive "$script_path/system/." "/."
+shopt -s globstar
+pushd "${pkg_root}"
+for f in **/*.*; do
+    sudo install --mode=644 "${pkg_root}/${f}" "${sys_root}/${f}"
+done
+popd
 
 # enable and start the systemd units
-systemctl is-enabled --quiet rke2-server.service || sudo systemctl enable rke2-server.service
+# systemctl is-enabled --quiet rke2-server.service || sudo systemctl enable rke2-server.service
 systemctl is-active --quiet rke2-server.service || sudo systemctl start rke2-server.service
 
 # copy authentication for installed server to user's config
